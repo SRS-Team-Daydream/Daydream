b4e441316378c483eafaad8eea53bfa88b074a39
ketexon <zane.a.s.clark@gmail.com>
2023-02-14 13:37:17 -0800

    Added inventory, equippable, and inventory item SOs. Added StatMods structure.

---
+++ Assets/Scenes/Shared/Scripts/ScriptableObjects/DataStructures/Inventory/EquippableSO.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Daydream
+{
+    [System.Serializable]
+    public enum EquipSlot { 
+        Weapon,
+        Accessory
+    }
+
+    [CreateAssetMenu(fileName = "Equippable", menuName = "Inventory/Equippable")]
+    public class EquippableSO : InventoryItemSO
+    {
+        [SerializeField]
+        public StatMods StatMods;
+
+        /// TODO
+        //[SerializeField]
+        //public List<AbilitySO> AbilityList;
+
+        [SerializeField]
+        public EquipSlot EquipSlot;
+    }
+}
