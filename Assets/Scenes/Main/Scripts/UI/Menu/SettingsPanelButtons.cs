9df3e1dc4688eff895c7473cd6edaa1bd02fb022
ketexon <zane.a.s.clark@gmail.com>
2023-03-03 18:29:33 -0800

    started new menu layout

--- Assets/Scenes/Main/Scripts/UI/Menu/SettingsPanelButtons.cs
+++ Assets/Scenes/Main/Scripts/UI/Menu/SettingsPanelButtons.cs
@@ -6,14 +6,7 @@ namespace Daydream
 {
     public class SettingsPanelButtons : MonoBehaviour
     {
-        public void OnQuitPressed()
-        {
-#if UNITY_EDITOR
-            Debug.Log("Quit Button Pressed");
-#else
-            Application.Quit();
-#endif
-        }
+
 
 
     }
