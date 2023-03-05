e92aca545825a659acbfbbaa2aa9200b2b254d3b
Anbu V <anbahalath@gmail.com>
2023-03-05 12:10:36 -0800

    setup raycast interact
    pro tip: dont use 3d physics for 2d T_T

--- Assets/Scenes/Shared/Scripts/Player.cs
+++ Assets/Scenes/Shared/Scripts/Player.cs
@@ -12,12 +12,49 @@ namespace Daydream
         [System.NonSerialized]
         public Controls Controls;
 
+        bool _interact = false;
+        [SerializeField] float _interactdist = 10f;
+
+        [SerializeField]
+        LayerMask mask;
+
+        
+
         private void Awake()
         {
             PlayerInput = GetComponent<PlayerInput>();
             Controls = new Controls();
             PlayerInput.actions = Controls.asset;
         }
+
+        void Start()
+        {
+            Controls.Gameplay.Action.started += OnInteract;
+        }
+
+        private void OnDestroy()
+        {
+            Controls.Gameplay.Action.started -= OnInteract;
+        }
+        public void Interact()
+        {
+            int _layermask = 1 << 7;
+
+            RaycastHit2D _hit;
+
+            _hit = Physics2D.Raycast(transform.position, transform.right, _interactdist, _layermask);
+
+            if (_hit.collider != null)
+            {
+                Debug.DrawRay(transform.position, transform.right * _hit.distance, Color.yellow);
+            }
+        }
+
+        void OnInteract(InputAction.CallbackContext ctx)
+        {
+            Interact();
+        }
+
 
     }
 }
