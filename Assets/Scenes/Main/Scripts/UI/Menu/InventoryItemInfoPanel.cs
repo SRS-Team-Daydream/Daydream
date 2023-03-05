07b12d2f373fc876012a01d1dafb4091656c2224
ketexon <zane.a.s.clark@gmail.com>
2023-03-04 19:27:51 -0800

    added info panel to inventory

---
+++ Assets/Scenes/Main/Scripts/UI/Menu/InventoryItemInfoPanel.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+namespace Daydream
+{
+    public class InventoryItemInfoPanel : MonoBehaviour
+    {
+        [SerializeField]
+        new TMP_Text name;
+
+        [SerializeField]
+        Image image;
+
+        [SerializeField]
+        TMP_Text description;
+
+        void Reset()
+        {
+            TMP_Text[] tmps = GetComponentsInChildren<TMP_Text>();
+            foreach(var tmp in tmps)
+            {
+                string nameLower = tmp.gameObject.name.ToLower();
+                if (nameLower.EndsWith("name"))
+                {
+                    name = tmp;
+                }
+                else if (nameLower.EndsWith("description"))
+                {
+                    description = tmp;
+                }
+            }
+            image = GetComponentInChildren<Image>();
+        }
+
+        public void SetInventoryItem(InventoryItemSO inventoryItem)
+        {
+            name.text = inventoryItem.Name;
+            image.sprite = inventoryItem.Sprite;
+            description.text = inventoryItem.Description;
+        }
+    }
+}
--- Assets/Scenes/Main/Scripts/UI/Menu/InventoryItemUI.cs
+++ Assets/Scenes/Main/Scripts/UI/Menu/InventoryItemUI.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Daydream
 {
-    public class InventoryItemUI : MonoBehaviour
+    public class InventoryItemUI : MonoBehaviour, ISelectHandler
     {
         [SerializeField]
         Image _image;
@@ -13,9 +14,17 @@ namespace Daydream
         [System.NonSerialized]
         public InventoryItemSO InventoryItem;
 
+        [System.NonSerialized]
+        public InventoryMenu InventoryMenu;
+
         void Start()
         {
             _image.sprite = InventoryItem.Sprite;
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            InventoryMenu.SelectItem(InventoryItem);
         }
     }
 }
