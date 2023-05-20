d98c22cb19b862c98e2d99b4c451d0b6b7043de8
ketexon <xketexon@gmail.com>
2023-05-19 19:53:03 -0700

    added unity object event

---
+++ Assets/Scenes/Shared/Scripts/ScriptableObjects/Events/UnityObjectEventSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Daydream
+{
+    [CreateAssetMenu(fileName = "UnityObjectEvent", menuName = "Events/(UnityEngine.Object)")]
+    public class UnityObjectEventSO : EventSO<Object>
+    { }
+}
